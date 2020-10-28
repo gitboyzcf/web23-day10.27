2e5702d8e5b5fea58e54b26a5431df366724ccfb
gitboyzcf <2313818945@qq.com>
2020-10-28 12:43:40 +0800

    更新

--- server.js
+++ server.js
@@ -20,56 +20,66 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())  //将表单数据解析成json格式
 
 
-app.engine('html',ejs.__express);//注册 html 模板引擎
-app.set('view engine', 'html');//配置模板引擎为html
+// app.engine('html',ejs.__express);//注册 html 模板引擎
+// app.set('view engine', 'html');//配置模板引擎为html
 
 // 配置模板引擎
-// app.set("view engine","ejs");
+app.set("view engine","ejs");
 // 配置模板所在目录
 app.set("views",__dirname+"/views");
 // 配置静态文件路径
 app.use(express.static("public"));
 
-
-// 配置路由
-app.get("/index",function(req,res){
-    
-    goods.find().then((result)=>{
-        console.log(result);
-        res.render("index",{goods:result});
-    })
-})
-// 拦截
-// app.use(function(req,res,next){
-//     // let userId = localStorage.getItem("userId");
-//     // console.log(userId);
-//     // if(userId){
-
-//     // }
-// })
 app.get("/",function(req,res){
-    console.log(app.locals);
+    // console.log(app.locals);
     res.render("login");
 })
+
 app.post("/login",function(req,res){
     uLogin.find(req.body).then((result)=>{
-        // console.log(result);
         if(result.length == 0){
             console.log("您的用户不存在，请重新输入");
+            res.redirect("http://192.172.1.33:3000");
         }else{
             console.log("登录成功");
-            // localStorage.setItem("userId",req.body._id);
-
+            // 存储该用户
+            console.log(result[0]._id)
+            app.locals.userId=result[0]._id; // 定义可以应用整个生命周期中使用的变量
+            app.locals.userName=result[0].username;
             
             res.redirect("/index");
         }
     })
 })
 
-app.get("/lll",(req,res)=>{
-    app.locals.username="123456"
-    res.send("ok")
+// 注册
+app.post("/user_register",async (req,res)=>{
+    let userObj = req.body;
+    let result = await uLogin.create(userObj);
+    console.log(result);
+    res.redirect("http://192.172.1.33:3000");
+});
+
+// 拦截---------------
+app.use(function(req,res,next){
+    let userId = app.locals.userId;
+    if(!userId){
+        res.render("login");
+    }else{
+        next();
+    }
+});
+
+// 配置路由
+app.get("/index",function(req,res){
+    
+    goods.find().then((result)=>{
+        // console.log(result);
+        res.render("index",{goods:result});
+    })
 })
+
+
 // 添加商品
 app.post("/addGoods",(req,res)=>{
     // 添加数据
@@ -96,7 +106,7 @@ app.post("/goodsUpdate",(req,res)=>{
         console.log(result);
         res.redirect("/index");
     })
-})
+});
 // 删除商品
 app.get("/deleteGoods",(req,res)=>{
     // 通过query获取get提交的数据
@@ -105,7 +115,7 @@ app.get("/deleteGoods",(req,res)=>{
         res.redirect("/index");
     })
     
-})
+});
 // 查找商品
 app.get("/findGoods",(req,res)=>{
     let goodsName = path.basename(req.url).split("?")[1].split("=")[1];
@@ -115,7 +125,17 @@ app.get("/findGoods",(req,res)=>{
         // console.log(result);
         res.render("index",{goods:result});
     })
+});
+// 退出
+app.get("/signOut",(req,res)=>{
+    // 防止下次登录还在线
+    app.locals.userId = null;
+    app.locals.userName = null;
+    res.redirect("http://192.172.1.33:3000");
 })
+
+
+
 
 
 
